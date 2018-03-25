ead4f210ee76f6ed78a0515cf732a30fda7e20f7
arnav <arnavgup@andrew.cmu.edu>
2018-03-25 06:47:01 -0400

    client controller and testing

--- ZWPA/test/controllers/clients_controller_test.rb
+++ ZWPA/test/controllers/clients_controller_test.rb
@@ -1,49 +1,65 @@
-# require 'test_helper'
+require 'test_helper'
+class ClientsControllerTest < ActionDispatch::IntegrationTest
 
-# class ClientsControllerTest < ActionController::TestCase
-#   setup do
-#     @client = clients(:one)
-#   end
+  setup do 
+    create_clients
+  end
+  
+  teardown do
+      delete_clients
+  end
 
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#     assert_not_nil assigns(:clients)
-#   end
+  test "should get index" do
+    get clients_url
+    assert_response :success
+    assert_not_nil assigns(:clients)
+  end
 
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
+  test "should get new" do
+    get new_client_url
+    assert_response :success
+  end
 
-#   test "should create client" do
-#     assert_difference('Client.count') do
-#       post :create, client: { active: @client.active, city: @client.city, contact_email: @client.contact_email, contact_first_name: @client.contact_first_name, contact_last_name: @client.contact_last_name, contact_phone: @client.contact_phone, contact_title: @client.contact_title, name: @client.name, state: @client.state, street_1: @client.street_1, street_2: @client.street_2, zip: @client.zip }
-#     end
+  test "should create a new client" do
+    assert_difference('Client.count', 1) do
+      post clients_url, params: { client: { name: "pnc", street_1: "123", street_2: "234", city: "Pittsburgh", state: "PA", zip: "11111", contact_phone: "1234566789", contact_email: "pnc@pnc.com", contact_title: "manager", contact_first_name: "bob", contact_last_name: "rob", active: true } }
+    end
+    assert_redirected_to client_url(assigns(:client))
+  end
 
-#     assert_redirected_to client_path(assigns(:client))
-#   end
+  test "should not create a new client with invalid params" do
+    post clients_url, params: { client: { name: "pnc", street_1: "123", street_2: "234", city: "Pittsburgh", state: "PA", zip: "11111", contact_phone: "66789", contact_email: "pnc@pnc.com", contact_title: "manager", contact_first_name: "bob", contact_last_name: "rob", active: true } }
+    assert_template :new
+  end
 
-#   test "should show client" do
-#     get :show, id: @client
-#     assert_response :success
-#   end
 
-#   test "should get edit" do
-#     get :edit, id: @client
-#     assert_response :success
-#   end
+  test "should show client" do
+    get client_url(@phipps)
+    assert_equal "Phipps Conservatory", assigns(:client).name
+    assert_response :success
+  end
 
-#   test "should update client" do
-#     patch :update, id: @client, client: { active: @client.active, city: @client.city, contact_email: @client.contact_email, contact_first_name: @client.contact_first_name, contact_last_name: @client.contact_last_name, contact_phone: @client.contact_phone, contact_title: @client.contact_title, name: @client.name, state: @client.state, street_1: @client.street_1, street_2: @client.street_2, zip: @client.zip }
-#     assert_redirected_to client_path(assigns(:client))
-#   end
+  test "should get edit" do
+    get edit_client_url(@phipps)
+    assert_not_nil assigns(:client)
+   assert_equal "Phipps Conservatory", assigns(:client).name
+    assert_response :success
+  end
 
-#   test "should destroy client" do
-#     assert_difference('Client.count', -1) do
-#       delete :destroy, id: @client
-#     end
+  test "should update a client" do
+    patch client_url(@phipps), params: { client: { name: "pnc", street_1: "123", street_2: "234", city: "Pittsburgh", state: "PA", zip: "11111", contact_phone: "1231231234", contact_email: "pnc@pnc.com", contact_title: "manager", contact_first_name: "bob", contact_last_name: "rob", active: true} }
+    assert_redirected_to client_url(@phipps)
+    assert_equal "pnc is updated.", flash[:notice]    
+  end
 
-#     assert_redirected_to clients_path
-#   end
-# end
+  test "should fail to update client with invalid params" do
+    patch client_url(@phipps), params: { client: { name: "pnc", street_1: "123", street_2: "234", city: "Pittsburgh", state: "PA", zip: "11111", contact_phone: "66789", contact_email: "pnc@pnc.com", contact_title: "manager", contact_first_name: "bob", contact_last_name: "rob", active: true} }
+    assert_template :edit
+  end
+
+  test "should destroy client" do
+    assert_difference('Client.count', -1) { delete client_path(@cmu) }
+    assert_redirected_to clients_path
+  end
+
+end
