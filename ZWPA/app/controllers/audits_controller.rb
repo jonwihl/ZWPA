95cec15b26e867f090bdd83991e0d0a14498b53e
jonwihl <jonathanwihl@gmail.com>
2018-04-02 00:16:47 -0400

    Minor view changes; Questionnaire views

--- ZWPA/app/controllers/audits_controller.rb
+++ ZWPA/app/controllers/audits_controller.rb
@@ -53,6 +53,6 @@ class AuditsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def audit_params
-      params.require(:audit).permit(:name, :audit_type, :start_date, :end_date, :active, :references, :references, :references)
+      params.require(:audit).permit(:name, :audit_type, :start_date, :end_date, :active, :client_id, :user_id, :questionnaire_id)
     end
 end
